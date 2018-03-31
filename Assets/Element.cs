574ab46af1fc1614390e47b61e0d4a736626dc91
ajung <cwhtha@gmail.com>
2018-03-31 20:09:26 +0900

    0331 mineSweeper(1)

---
+++ Assets/Element.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour {
+	public bool mine;
+
+	public Sprite[] emptyTextures;
+	public Sprite mineTexture;
+
+	// Use this for initialization
+	void Start () {
+		mine = Random.value < 0.15;
+//		loadTexture(1);
+	}
+
+	public void loadTexture(int adjacentCount){
+		if (mine)
+			GetComponent<SpriteRenderer> ().sprite = mineTexture;
+		else
+			GetComponent<SpriteRenderer> ().sprite = emptyTextures [adjacentCount];
+	}
+
+	public bool isCovered(){
+		return GetComponent<SpriteRenderer> ().sprite.texture.name = "ice";
+	}
+}
