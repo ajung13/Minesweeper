2cf0d4d9bc03cbd1bcbff8a779faa135a1f97864
ajung <cwhtha@gmail.com>
2018-03-31 20:41:58 +0900

    0331 minesweeper(2)

--- Assets/Element.cs
+++ Assets/Element.cs
@@ -12,6 +12,23 @@ public class Element : MonoBehaviour {
 	void Start () {
 		mine = Random.value < 0.15;
 //		loadTexture(1);
+
+		int x = (int)transform.position.x;
+		int y = (int)transform.position.y;
+		myGrid.elements [x, y] = this;
+	}
+
+	void OnMouseUpAsButton(){
+		if (mine) {
+//			myGrid.uncoverMines ();
+//			print ("YOU LOSE");
+			loadTexture(2);
+		} else {
+			int x = (int)transform.position.x;
+			int y = (int)transform.position.y;
+//			loadTexture (myGrid.adjacentMines (x, y));
+			loadTexture(1);
+		}
 	}
 
 	public void loadTexture(int adjacentCount){
@@ -22,6 +39,6 @@ public class Element : MonoBehaviour {
 	}
 
 	public bool isCovered(){
-		return GetComponent<SpriteRenderer> ().sprite.texture.name = "ice";
+		return (GetComponent<SpriteRenderer> ().sprite.texture.name == "ice");
 	}
 }
---
+++ Assets/myGrid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class myGrid {
+	public static int w = 10;
+	public static int h = 13;
+	public static Element[,] elements = new Element[w, h];
+
+	public static void uncoverMines(){
+		foreach (Element elem in elements) {
+			if (elem.mine)
+				elem.loadTexture (0);
+		}
+	}
+
+	public static bool mineAt(int x, int y){
+		if (x >= 0 && y >= 0 && x < w && y < h)
+			return elements [x, y].mine;
+		return false;
+	}
+
+	public static int adjacentMines(int x, int y){
+		int count = 0;
+
+		if (mineAt (x, y + 1))
+			++count;
+		if (mineAt (x + 1, y + 1))
+			++count;
+		if (mineAt (x + 1, y))
+			++count;
+		if (mineAt (x + 1, y - 1))
+			++count;
+		if (mineAt (x, y - 1))
+			++count;
+		if (mineAt (x - 1, y - 1))
+			++count;
+		if (mineAt (x - 1, y))
+			++count;
+		if (mineAt (x - 1, y + 1))
+			++count;
+
+		return count;
+	}
+}
